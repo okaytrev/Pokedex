af993f5694ce03ebcc7bf48b4a56f6f5be7400a7
trevar.hupf <trevabooo@gmail.com>
2021-11-14 13:32:08 -0700

    Added more logic for evolution button

--- Pokedex.py
+++ Pokedex.py
@@ -11,44 +11,56 @@ import tkinter as tk
 import csv
 from PIL import ImageTk, Image
 
-
+############################### INITIAL_FORM_CREATION #####################################
+#path to folder that contains pokemon images
 path = "C:\\Users\\TrevarHupf\\Documents\\Scripts\\Pokedex\\PokeImages\\"
-#Create form
-form = tk.Tk()
-form.title("Pokedex")
-#Set size of form
-form.geometry("746x542")
-#Sets background of form
-background = PhotoImage(file = "PokedexImg.png")
+form = tk.Tk() #create main form
+form.title("Pokedex") #Title form
+form.geometry("746x542") #Sets form size
+background = PhotoImage(file = "PokedexImg.png") #sets background image
 #Create canvas
-canvas1 = Canvas(form,width=746, height=542)
+canvas1 = Canvas(form,width=746, height=542) 
 canvas1.pack(fill = "both", expand = True)
-canvas1.create_image( 0, 0,image= background, anchor="nw")
-canvas1.configure(bg='white')
+canvas1.create_image( 0, 0,image= background, anchor="nw") #Sets images to background image
+canvas1.configure(bg='white') #background color
+###########################################################################################
 
+#function to set image to a more usable format using PIL 
+def set_image(picture_number):
+    return (ImageTk.PhotoImage(Image.open(path + picture_number+".jpg")))
+    
+#Functin for evolve button click
 def button_evolve():
-    global original,evolve1, evolve2, arrow1, arrow2
-    evolveform = Toplevel(form)
-    evolveform.geometry("800x650")
-    evolveform.configure(bg='white')
+    global original,evolve1, evolve2, arrow1, arrow2 #set global variables ~ required for multiple images in TKINTER
+    #Create a second form that will contain evolution images
+    #Store input of Poke search
     string = inputtxt.get()
-    mystring = string.capitalize()
-    evolveform.title("Evolution of " + string)
+    mystring = string.capitalize() #capitalize first letter to match CSV
     with open ("pokemon.csv", "r", encoding='utf-8') as csv_file: #utf-8 to avoid strange symbols, reads the CSV
-        reader = csv.reader(csv_file, delimiter=',') 
-        for lines in reader:
-            if lines[1] == mystring:
-                first_evolution = lines[0]
-                a,b = lines[13].split(',')
-                original = ImageTk.PhotoImage(Image.open(path + first_evolution+".jpg"))
-                Label(evolveform,image=original).place(x='250',y='0')
-                evolve1 = ImageTk.PhotoImage(Image.open(path + a +".jpg"))
-                Label(evolveform,image=evolve1).place(x='500',y='400')
-                evolve2 = ImageTk.PhotoImage(Image.open(path + b +".jpg"))
-                Label(evolveform,image=evolve2).place(x='0',y='400')
-                
-
-
+        reader = csv.reader(csv_file, delimiter=',') #Sets reader variable, dlimiter is comma
+        for lines in reader: #loop through columns of CSV
+            if lines[1] == mystring: #if column [1] (second column that contains pokemon names) equals the string the user searches
+                evolveform = Toplevel(form)
+                evolveform.geometry("800x650")
+                evolveform.configure(bg='white')
+                evolveform.title("Evolution of " + string) #Set title
+                first_evolution = lines[0]   
+                original = set_image(first_evolution)
+                if ',' in lines[13]: #searches for comma in csv, this identifies that pokemon evolves twice
+                    a,b = lines[13].split(',')
+                    Label(evolveform,image=original).place(x='250',y='0')
+                    evolve1 = set_image(a)
+                    Label(evolveform,image=evolve1).place(x='500',y='400')
+                    evolve2 = set_image(b)
+                    Label(evolveform,image=evolve2).place(x='0',y='400')
+                elif lines[13] != "0": #if the evolution column does not equal zero, this assumes that pokemon only evolve once
+                    Label(evolveform,image=original).place(x='250',y='0')
+                    evolve1 = set_image(lines[13])
+                    Label(evolveform,image=evolve1).place(x='250',y='400')
+                else: #assumes that the pokemon does not evolve
+                    evolveform.title(string + " is fully evolved")
+                    Label(evolveform,image=original).place(x='250',y='200')
+                                    
 #Button press function 
 def button_search():
     global pokeimage
@@ -60,22 +72,13 @@ def button_search():
         reader = csv.reader(csv_file, delimiter=',') 
         for lines in reader:
             if lines[1] == mystring:
-                type1 = lines[2]
-                type2 = lines[3] 
-                hp = lines[5]
-                attack = lines[6]
-                defense = lines[7]
-                spatk = lines[8]
-                spdef = lines[9]
-                speed = lines[10]
-                legendary = lines[12]
-                image = lines[0]
-        pokeimage = ImageTk.PhotoImage(Image.open(path + image +".jpg"))
+                type1 = lines[2], type2 = lines[3], hp = lines[5], attack = lines[6], defense = lines[7] #setting stats variables based on pokmeon selected
+                spatk = lines[8], spdef = lines[9],speed = lines[10],legendary = lines[12],image = lines[0] #two lines for readablility
+        pokeimage = set_image(image)
         #pokeimage = PhotoImage(file = pokeimage1)
-        pokelabel = Label(form,image=pokeimage)
+        pokelabel = Label(form,image=pokeimage) #calls image function 
         canvas1.create_window(573,273,window=pokelabel)
- 
-    #Edit form
+    ############################### Updates labels based on the stats of pokeomon #####################################
     typelabel = tk.Label(form,bg='white',text=type1)
     typelabel2 = tk.Label(form,bg='white',text=type2)
     hplabel = tk.Label(form,bg='white',text=hp)
@@ -95,9 +98,9 @@ def button_search():
     canvas1.create_window(145,305,window=spdeflabel)
     canvas1.create_window(255,185,window=speedlabel)
     canvas1.create_window(255,205,window=leglabel)
+    ######################################################################################################################
 
-
-# TextBox Creation
+######################################### MORE GUI STUFF #####################################################
 inputtxt = tk.Entry(form)
 pokelabel = tk.Label(form,bg='white',text="Pokemon: ")
 type_label = tk.Label(form,bg='white',text="Type 1: ")
@@ -109,8 +112,8 @@ spatk_label = tk.Label(form,bg='white',text="Sp Attack: ")
 spdef_label = tk.Label(form,bg='white',text="Sp Defense: ")
 speed_label = tk.Label(form,bg='white',text="Speed: ")
 leg_label = tk.Label(form,bg='white',text="Legendary: ")
-button_pokesearch = tk.Button(form,text="Pokemon Search",bg='orange', command = button_search)
-button_evolve = tk.Button(form,text="Evolve",bg='orange', command = button_evolve)
+button_pokesearch = tk.Button(form,text="Pokemon Search",bg='orange', command = button_search) #button calls search_button function
+buttonevolve = tk.Button(form,text="Evolve",bg='orange', command = button_evolve) #button calls button_evolve function
 canvas1.create_window(90,165,window=pokelabel)
 canvas1.create_window(79,185,window=type_label)
 canvas1.create_window(79,205,window=type2_label)
@@ -123,7 +126,8 @@ canvas1.create_window(205,185,window=speed_label)
 canvas1.create_window(215,205,window=leg_label)
 canvas1.create_window(195, 165, window=inputtxt)
 canvas1.create_window(150, 480, height=70,width=130, window=button_pokesearch)
-canvas1.create_window(510, 490, height=55,width=130, window=button_evolve)
-
+canvas1.create_window(510, 490, height=55,width=130, window=buttonevolve)
+###############################################################################################################
+#initiate main form
 form.mainloop()
 
